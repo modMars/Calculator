05685dde775641e9a54a7d68064821fd24879a6a
modMars <malejandrosalcido@gmail.com>
2022-09-02 00:29:04 -0600

    Add calculator structure and style

---
+++ app.js
@@ -0,0 +1,10 @@
+/*Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
+add
+subtract
+multiply
+divide*/
+
+function operate(num1,num2,op){
+    
+}
+
