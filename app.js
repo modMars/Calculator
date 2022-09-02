b129c793266ed77193238a077a822b09e129c0d7
modMars <malejandrosalcido@gmail.com>
2022-09-02 02:15:23 -0600

    Add references and display updates

--- app.js
+++ app.js
@@ -5,6 +5,50 @@ multiply
 divide*/
 
 function operate(num1,num2,op){
-    
+
+}
+function ClearDisplay(){
+    display.textContent = '';
 }
 
+function DeleteDisplay(){
+    display.textContent = display.textContent.slice(0,-1);
+}
+
+var input = "";
+var operator = "";
+const num = document.querySelectorAll('[data-num]');
+const op = document.querySelectorAll('[data-op]');
+const display = document.querySelector('.display');
+const clear = document.getElementById('clear');
+const del = document.getElementById('delete')
+
+num.forEach(e => {
+    e.addEventListener('click', (e) => {
+        input = (e.target.textContent);
+        console.log(input)
+        if(display.textContent.length < 11)
+        display.append(input);
+        else
+        return
+    });
+});
+
+op.forEach(e => {
+    e.addEventListener('click',(e) =>{
+        operator = (e.target.textContent);
+        console.log(display.textContent);
+        if(display.textContent != '')
+        {
+            if(display.textContent.length < 11)
+            display.append(operator);
+            else
+            return
+        }
+        else 
+        return;
+    })
+})
+
+clear.addEventListener('click',ClearDisplay);
+del.addEventListener('click',DeleteDisplay)
